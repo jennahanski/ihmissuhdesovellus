29abbd16547c2b3885e57b3de90d32d11d552a7b
Jenna <jennahanski@hotmail.com>
2023-01-29 14:12:51 +0200

    Add first version of schema.sql

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users,
+    name TEXT
+);
+
+CREATE TABLE lists (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    game_id INTEGER REFERENCES games,
+    name TEXT
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    list_id INTEGER REFERENCES lists,
+    comment TEXT,
+    grade INTEGER
+);
+
